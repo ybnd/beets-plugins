89dd133e32d0f7c72e8243ec847989315860fbfd
ybnd <ybnd@tuta.io>
2020-01-27 13:11:05 +0100

    Look around & plan how to handle https://github.com/beetbox/beets/issues/2224

---
+++ debug_beet_replaygain.py
@@ -0,0 +1,10 @@
+from __future__ import division, absolute_import, print_function
+from beets.ui import main
+
+if __name__ == "__main__":
+    main(
+        [
+            '-c', 'config.yaml',
+            'replaygain', '-f', 'Rosetta'
+        ]
+    )
